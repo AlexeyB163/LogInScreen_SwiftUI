e987ac84fcdce40ba836b6157eaa890067ce1ce2
AlexeyB163 <sourceaisa@mail.ru>
2022-04-06 09:31:00 +0400

    Initial Commit

---
+++ LogInScreen_SwiftUI/LogInScreen_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogInScreen_SwiftUIApp.swift
+//  LogInScreen_SwiftUI
+//
+//  Created by User on 06.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct LogInScreen_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
